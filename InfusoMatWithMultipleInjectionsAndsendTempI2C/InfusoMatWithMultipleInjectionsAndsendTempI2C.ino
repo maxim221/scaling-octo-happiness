0e54315a9a14cc5f4d3e152e25b4b150138f46d4
Maxim Kamensky <maxim221@gmail.com>
2020-10-11 19:53:09 +0300

    test

--- InfusoMatWithMultipleInjectionsAndsendTempI2C/InfusoMatWithMultipleInjectionsAndsendTempI2C.ino
+++ InfusoMatWithMultipleInjectionsAndsendTempI2C/InfusoMatWithMultipleInjectionsAndsendTempI2C.ino
@@ -642,7 +642,7 @@ void procedureScreen(){
     selectScreen = 7;
     screenCleared = false;
   }
-  evaluateTDifference();
+  evaluateTDifference();//test
   checkAlarm();
    
   if ((millis() / procedureScreenSwitchRate) % 2 == 0) {
